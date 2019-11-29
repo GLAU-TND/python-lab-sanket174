d04b56834fa5893e6722bc81901f8701ebf10547
sanket174 <sanketkumar174@gmail.com>
2019-11-29 17:01:18 +0530

    Program 3

--- program3.py
+++ program3.py
@@ -1,9 +1,7 @@
 try:
     a=int(input())
 
-except ValueError as e:
+except ValueError :
     raise ValueError("ValueError occured ")
-except KeyboardInterrupt as e:
+except KeyboardInterrupt :
     raise KeyboardInterrupt("Keyboard interrupt occur")
-except EOFError as e:
-    raise EOFError("EOFError occured")
